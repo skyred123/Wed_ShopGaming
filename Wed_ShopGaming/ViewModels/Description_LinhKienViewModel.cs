146d45272fef87f8ecb62bd08c2e13b080f19c95
skyred123 <94950718+skyred123@users.noreply.github.com>
2023-03-27 12:49:37 +0700

    sua loi

---
+++ Wed_ShopGaming/ViewModels/Description_LinhKienViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wed_ShopGaming.Models.Entity;
+
+namespace Wed_ShopGaming.ViewModels
+{
+    public class Description_LinhKienViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCheck { get; set; }
+    }
+    public class ListDescription_LinhKienViewModel
+    {
+        public List<Description_LinhKienViewModel> description_LinhKiens { get; set; } //= new List<bool>();
+    }
+}
